75ce4b594f98fd5621bb550fd6de56a0a960d65f
tardisdriver <stark.tracy@gmail.com>
2017-06-27 17:17:15 -0500

    fixed typo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mmongodb://tardisdriver:Temple45evil@ds139362.mlab.com:39362/blog-challenge';
+                      'mongodb://tardisdriver:Temple45evil@ds139362.mlab.com:39362/blog-challenge';
 exports.PORT = process.env.PORT || 8080;
